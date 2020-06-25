2c9f0cca9fd6aeb5d0b3a63216ff943aaf1af42f
Joachim Kret <joachimkret@macbook.pro>
2020-06-25 16:39:34 +0200

    Added example.

---
+++ FlowCoordinators/FlowCoordinators/TemplateViewController.swift
@@ -0,0 +1,100 @@
+//
+//  Created by Joachim Kret on 12/03/2020.
+//  Copyright © 2020 Joachim Kret. All rights reserved.
+//
+
+import UIKit
+
+class TemplateViewController: UIViewController {
+    
+    @IBOutlet private weak var pushButton: UIButton!
+    @IBOutlet private weak var popButton: UIButton!
+    
+    @IBOutlet private weak var pushHeadButton: UIButton!
+    @IBOutlet private weak var popHeadButton: UIButton!
+    
+    @IBOutlet private weak var replaceCurrentForewardButton: UIButton!
+    @IBOutlet private weak var replaceCurrentBackwardButton: UIButton!
+    
+    @IBOutlet private weak var replaceHeadForewardButton: UIButton!
+    @IBOutlet private weak var replaceHeadBackwardButton: UIButton!
+    
+    @IBOutlet private weak var rawViewControllerButton: UIButton!
+    @IBOutlet private weak var dumpChainingButton: UIButton!
+    
+    @IBOutlet private weak var backButtonSwitch: UISwitch!
+    @IBOutlet private weak var interactiveBackSwitch: UISwitch!
+    
+    @IBAction func push() {
+        pushHandler?()
+    }
+    
+    @IBAction func pop() {
+        popHandler?()
+    }
+    
+    @IBAction func pushHead() {
+        pushHeadHandler?()
+    }
+    
+    @IBAction func popHead() {
+        popHeadHandler?()
+    }
+    
+    @IBAction func replaceCurrentForeward() {
+        replaceCurrentForewardHandler?()
+    }
+    
+    @IBAction func replaceCurrentBackward() {
+        replaceCurrentBackwardHandler?()
+    }
+    
+    @IBAction func replaceHeadForeward() {
+        replaceHeadForewardHandler?()
+    }
+    
+    @IBAction func replaceHeadBackward() {
+        replaceHeadBackwardHandler?()
+    }
+    
+    @IBAction func rawViewController() {
+        rawViewControllerHandler?()
+    }
+    
+    @IBAction func dumpChaining() {
+        dumpChainingHandler?()
+    }
+    
+    @IBAction func backButtonChanged() {
+        backButtonChangedHandler?(backButtonSwitch.isOn)
+    }
+    
+    @IBAction func interactiveBackChanged() {
+        interactiveBackChangedHandler?(interactiveBackSwitch.isOn)
+    }
+    
+    var pushHandler: (() -> ())?
+    var popHandler: (() -> ())?
+    
+    var pushHeadHandler: (() -> ())?
+    var popHeadHandler: (() -> ())?
+    
+    var replaceCurrentForewardHandler: (() -> ())?
+    var replaceCurrentBackwardHandler: (() -> ())?
+    
+    var replaceHeadForewardHandler: (() -> ())?
+    var replaceHeadBackwardHandler: (() -> ())?
+    
+    var rawViewControllerHandler: (() -> ())?
+    var dumpChainingHandler: (() -> ())?
+    
+    var backButtonChangedHandler: ((Bool) -> ())?
+    var interactiveBackChangedHandler: ((Bool) -> ())?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
---
+++ FlowCoordinators/FlowCoordinators/ViewFlowController.swift
@@ -0,0 +1,153 @@
+//
+//  Created by Joachim Kret on 22/06/2020.
+//  Copyright © 2020 Joachim Kret. All rights reserved.
+//
+
+import UIKit
+
+enum ViewWireframe: FlowEvent {
+    case push
+    case pop
+    case replaceForeward
+    case repalceBackward
+}
+
+final class ViewFlowController: NavigationFlowController, CustomDebugStringConvertible {
+    
+    private let isRoot: Bool
+    private var index: Int
+    
+    private var allowsInteractiveTransition = true
+    private var allowsBackButton = true
+    
+    var debugDescription: String {
+        return "ViewFlowCoordinator: { isRoot: \(isRoot), index: \(index) }"
+    }
+    
+    init(isRoot: Bool = true, index: Int = 0) {
+        self.isRoot = isRoot
+        self.index = index
+    }
+    
+    deinit {
+        print("['] deinit flow controller: \(index)")
+    }
+    
+    override func shouldAllowDismissFlowCoordinator(isInteractive: Bool) -> Bool {
+        if isInteractive {
+            let action = allowsInteractiveTransition ? "allowed" : "refused"
+            print("-> interactive dismiss \(action) for flow controller:", index)
+            return allowsInteractiveTransition
+        } else {
+            let action = allowsBackButton ? "allowed" : "refused"
+            print("-> back dismiss \(action) for flow controller:", index)
+            return allowsBackButton
+        }
+    }
+    
+    override func willPresentFlowController(animated: Bool) {
+        print("-> will present flow controller:", index)
+    }
+    
+    override func didPresentFlowController(animated: Bool) {
+        print("-> did present flow controller:", index)
+    }
+    
+    override func didStartInteractiveTransition() {
+        print("-> did start interactive transition for flow controller:", index)
+    }
+    
+    override func didChangeInteractiveTransition(isCancelled: Bool) {
+        print("-> did change interactive transition for flow controller:", index)
+    }
+
+    override func didCompleteInteractiveTransition(isCancelled: Bool) {
+        print("-> did complete interactive transition for flow controller:", index)
+    }
+    
+    override func start() -> UIViewController {
+        registerWireframe()
+        return prepareViewController()
+    }
+    
+    private func prepareViewController() -> UIViewController {
+        let viewController = TemplateViewController()
+        viewController.pushHandler = { [weak self] in self?.presentNext() }
+        viewController.popHandler = { [ weak self] in self?.presentPrevious() }
+        viewController.pushHeadHandler = { [weak self] in self?.wireframe(ViewWireframe.push) }
+        viewController.popHeadHandler = { [weak self] in self?.wireframe(ViewWireframe.pop) }
+        viewController.replaceCurrentForewardHandler = { [weak self] in self?.replaceCurrentForeward() }
+        viewController.replaceCurrentBackwardHandler = { [weak self] in self?.replaceCurrentBackward() }
+        viewController.replaceHeadForewardHandler = { [weak self] in self?.wireframe(ViewWireframe.replaceForeward) }
+        viewController.replaceHeadBackwardHandler = { [weak self] in self?.wireframe(ViewWireframe.repalceBackward) }
+        viewController.rawViewControllerHandler = { [weak self] in self?.presentRawViewController() }
+        viewController.dumpChainingHandler = { [weak self] in self?.dumpChaining() }
+        viewController.backButtonChangedHandler = { [weak self] in self?.allowsBackButton = $0 }
+        viewController.interactiveBackChangedHandler = { [weak self] in self?.allowsInteractiveTransition = $0 }
+        viewController.title = "\(index)"
+        return viewController
+    }
+    
+    private func registerWireframe() {
+        
+        guard isRoot else { return }
+        
+        handle(ViewWireframe.self) { [weak self] (event) in
+            switch event {
+            case .push:
+                self?.presentNext()
+            case .pop:
+                self?.presentCurrent()
+            case .replaceForeward:
+                self?.replaceCurrentForeward()
+            case .repalceBackward:
+                self?.replaceCurrentBackward()
+            }
+        }
+    }
+    
+    private func presentRawViewController() {
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .red
+        push(viewController, completion: { print("-> raw UIViewController push completed") })
+    }
+    
+    private func presentNext() {
+        let flowController = createFlowController(isRoot: false)
+        let index = flowController.index
+        push(flowController, completion: { print("-> child flow controller push completed:", index) })
+    }
+    
+    private func presentPrevious() {
+        let index = self.index
+        popToPrevious(completion: { print("-> pop from flow controller completed:", index) })
+    }
+    
+    private func presentCurrent() {
+        let index = self.index
+        popToCurrent(completion: { print("-> pop to flow controller completed:", index) })
+    }
+    
+    private func replaceCurrentForeward() {
+        let flowController = createFlowController(isRoot: isRoot)
+        let isRoot = flowController.isRoot
+        let index = flowController.index
+        replace(with: flowController, animation: .push, completion: {
+            print("-> replaced \(isRoot ? "root" : "child") flow controller completed:", index)
+        })
+    }
+    
+    private func replaceCurrentBackward() {
+        let flowController = createFlowController(isRoot: isRoot)
+        let isRoot = flowController.isRoot
+        let index = flowController.index
+        replace(with: flowController, animation: .pop, completion: {
+            print("-> replaced \(isRoot ? "root" : "child") flow controller completed:", index)
+        })
+    }
+
+    private func createFlowController(isRoot: Bool) -> ViewFlowController {
+        let flowController = ViewFlowController(isRoot: isRoot, index: index + 1)
+        return flowController
+    }
+}
